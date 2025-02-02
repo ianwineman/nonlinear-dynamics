69423ac70ba41df207e2e1428690b8e9029e5ef2
ianwineman <ian@ianwineman.com>
2025-02-01 19:41:43 -0500

    Update exercise1_6.jl

--- exercises/exercise1_6.jl
+++ exercises/exercise1_6.jl
@@ -8,66 +8,71 @@ end
 vs = 0.0:0.1:2π
 
 points = [(θ,v) for θ=θs for v=vs]
-orbits = []
 
-for p=points
-	steps = [p]
-	for step=1:1000
-		s = standard_map(steps[end]...,k=1)
-		push!(steps,s)
-		if isapprox(s[1],p[1],atol=0.1) && isapprox(s[2],p[2],atol=0.1)
-			push!(orbits,step)
-			break
-		else
-			if step == 1000
-				push!(orbits,1000)
-			end
-		end
-	end
-end
-
-heatmap(
-	θs, vs, reshape(orbits, length(θs), length(vs))', 
-	c=cgrad(:default, [0.01, 0.02, 0.03, 0.1, 0.2, 0.3, 0.1]),
-	title="Orbits of Standard Map for k=1",
-	xguide="v",
-	yguide="θ"
-)
-
-savefig("plots/exercise1_6.png")
-
-#anim = @animate for k=0:0.01:5
-#    orbits = []
-#	for p=points
-#		steps = [p]
-#		for step=1:1000
-#			s = standard_map(steps[end]...,k=k)
-#			push!(steps,s)
-#			if isapprox(s[1],p[1],atol=0.1) && isapprox(s[2],p[2],atol=0.1)
-#				push!(orbits,step)
-#				break
-#			else
-#				if step == 1000
-#					push!(orbits,1000)
-#				end
+## START Single Plot ##
+#orbits = []
+#
+#for p=points
+#	steps = [p]
+#	for step=1:1000
+#		s = standard_map(steps[end]...,k=1)
+#		push!(steps,s)
+#		if isapprox(s[1],p[1],atol=0.1) && isapprox(s[2],p[2],atol=0.1)
+#			push!(orbits,step)
+#			break
+#		else
+#			if step == 1000
+#				push!(orbits,1000)
 #			end
 #		end
 #	end
-#    
-#    heatmap(
-#        0.0:0.01:2π, 0.0:0.01:2π, 
-#        reshape(orbits, length(θs), length(vs))',
-#        c=cgrad([:blue, :yellow, :red, :white], [0.0, 0.05, 0.1, 1.0]),
-#        title="Orbits of Standard Map for k=$k",
-#        xguide="v",
-#        yguide="θ"
-#    )
-#    println("k=$k")
 #end
 #
-#gif(anim, "standard_map.gif", fps=5)
+#heatmap(
+#	θs, vs, reshape(orbits, length(θs), length(vs))', 
+#	c=cgrad(:default, [0.01, 0.02, 0.03, 0.1, 0.2, 0.3, 0.1]),
+#	title="Orbits of Standard Map for k=1",
+#	xguide="v",
+#	yguide="θ"
+#)
+#
+#savefig("plots/exercise1_6.png")
+## END Single Plot ##
+
+
+## START gif ##
+anim = @animate for k=0:0.01:5
+    orbits = []
+	for p=points
+		steps = [p]
+		for step=1:1000
+			s = standard_map(steps[end]...,k=k)
+			push!(steps,s)
+			if isapprox(s[1],p[1],atol=0.1) && isapprox(s[2],p[2],atol=0.1)
+				push!(orbits,step)
+				break
+			else
+				if step == 1000
+					push!(orbits,1000)
+				end
+			end
+		end
+	end
+    
+    heatmap(
+        θs, vs, reshape(orbits, length(θs), length(vs))',
+        c=cgrad(:default, [0.01, 0.02, 0.03, 0.1, 0.2, 0.3, 0.1]),
+        title="Orbits of Standard Map for k=$k",
+        xguide="v",
+        yguide="θ"
+    )
+    println("k=$k")
+end
+
+gif(anim, "standard_map.gif", fps=5)
 # export GKSwstype=nul
 # https://github.com/JuliaPlots/Plots.jl/issues/3664#issuecomment-887365869
+## END gif ##
 
 
 

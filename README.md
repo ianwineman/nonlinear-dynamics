56eba2031a7d82ba8ac0525cb3e8334f26d408d8
ianwineman <ian@ianwineman.com>
2025-02-05 08:16:08 -0500

    complete exercise 1.8

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Nonlinear Dynamics
 [A Concise Introduction Interlaced with Code](https://link.springer.com/book/10.1007/978-3-030-91032-7)
 
+## Exercise 1.8
+[Code](https://github.com/ianwineman/nonlinear-dynamics/blob/main/exercises/exercise1_8.jl)
+
+![Exercise 1.8](plots/exercise1_8.gif)
+
 ## Exercise 1.7
 [Code](https://github.com/ianwineman/nonlinear-dynamics/blob/main/exercises/exercise1_7.jl)
 
--- exercises/exercise1_8.jl
+++ exercises/exercise1_8.jl
@@ -16,33 +16,43 @@ p = [10.0, 28.0, 8/3]
 prob = ODEProblem(lorenz63!, u0, tspan, p)
 sol = solve(prob)
 
-p1 = plot(
-	sol, idxs=(1,2,3),
-	xlim=[-30,30], ylim=[-30,30], zlim=[0,60],
-	label="",
-	linewidth=0.1,
-	c=:orange,
-)
+anim = @animate for z=0:0.1:50
+	p1 = plot(
+		sol, idxs=(1,2,3),
+		xlim=[-30,30], ylim=[-30,30], zlim=[0,60],
+		label="",
+		linewidth=0.1,
+		c=:orange,
+		xlabel="x", ylabel="y", zlabel="z"
+	)
 
-x = -30:0.1:30
-y = -30:0.1:30
-surface!(
-	x, y, (x, y) -> 25.0,
-	alpha=0.3,
-	c=:grey,
-	colorbar=false
-)
+	x = -30:0.1:30
+	y = -30:0.1:30
+	surface!(
+		x, y, (x, y) -> z,
+		alpha=0.3,
+		c=:grey,
+		colorbar=false
+	)
 
-p2 = scatter(
-	[u[1] for u=sol.u if isapprox(u[3],25.0,atol=0.1)],
-	[u[2] for u=sol.u if isapprox(u[3],25.0,atol=0.1)],
-	xlim=[-30,30], ylim=[-30,30],
-	c=:orange,
-	markerstrokewidth=0,
-	label="z=25.0"
-)
+	p2 = scatter(
+		[u[1] for u=sol.u if isapprox(u[3],z,atol=0.1)],
+		[u[2] for u=sol.u if isapprox(u[3],z,atol=0.1)],
+		xlim=[-30,30], ylim=[-30,30],
+		xlabel="x", ylabel="y",
+		c=:orange,
+		markerstrokewidth=0,
+		markersize=2,
+		label="z=$z",
+		legendposition=:topleft
+	)
 
-plot(
-	p1, p2,
-	plot_title="Lorenz-63"
-)
+	plot(
+		p1, p2,
+		plot_title="Lorenz-63"
+	)
+
+    println("z=$z")
+end
+
+gif(anim, "plots/exercise1_8.gif", fps=10)

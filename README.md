4f88c37f50be31d1a467e7b43086e3cd870e90e0
ianwineman <ian@ianwineman.com>
2025-02-16 08:12:31 -0500

    complete exercise 1.15

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Nonlinear Dynamics
 [A Concise Introduction Interlaced with Code](https://link.springer.com/book/10.1007/978-3-030-91032-7)
 
+## Exercise 1.15
+[Code](https://github.com/ianwineman/nonlinear-dynamics/blob/main/exercises/exercise1_15.jl)
+
+![Exercise 1.15](plots/exercise1_15.png)
+
 ## Exercise 1.17
 [Code](https://github.com/ianwineman/nonlinear-dynamics/blob/main/exercises/exercise1_17.jl)
 
---
+++ exercises/exercise1_15.jl
@@ -0,0 +1,61 @@
+using DifferentialEquations
+using LinearAlgebra
+using Plots
+
+function duffing!(du, u, p, t)
+	d = p
+	x, y = u
+
+	du[1] = y
+	du[2] = x - x^3 - d*y
+end
+
+function trajectory(u0)
+	tspan = (0.0,100.0)
+	p = 0.2
+
+	prob = ODEProblem(duffing!, u0, tspan, p)
+	sol = solve(prob)
+
+	return sol.u[end]
+end
+
+function attractor(u0, attractors)
+	for a=attractors
+		if isapprox(norm(trajectory(u0).-a), 0.0; atol=0.001)
+			return findfirst(x -> x == a, attractors)
+		end
+	end
+	return 0
+end
+
+function zs_of_points(points, attractors)
+	zs = zeros(Float64,length(points))
+	Threads.@threads for i=1:length(points)
+		zs[i] = attractor(points[i],attractors)
+	end
+	return zs
+end
+
+xs = range(-5,5, length=1000)
+ys = range(-5,5, length=1000)
+points = [[x,y] for x=xs for y=ys]
+zs = zs_of_points(points,[[-1.0,0.0],[1.0,0.0]])
+
+contour(
+   xs,ys,zs,
+   fill=true,
+   color=[:mediumaquamarine,:mediumpurple],
+   colorbar=:none,
+   xlabel="x", ylabel="y", title="Duffing oscillator \$(d=0.2)\$",
+   xlim=[-5,5], ylim=[-5,5],
+   xtick=[-5,-4,-3,-2,-1,0,1,2,3,4,5],
+   ytick=[-5,-4,-3,-2,-1,0,1,2,3,4,5],
+)
+scatter!(
+	[-1.0,1.0], [0.0,0.0], 
+	ms=4, msw=2, mc=[:mediumpurple,:mediumaquamarine], 
+	label=:none
+)
+
+savefig("plots/exercise1_15.png")

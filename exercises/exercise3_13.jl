2f9fe09327d472981a1553532fd554d9dbde5e69
Ian Wineman <130872935+ianwineman@users.noreply.github.com>
2025-10-17 10:05:47 -0400

    start exercise 3.13

---
+++ exercises/exercise3_13.jl
@@ -0,0 +1,32 @@
+using LinearAlgebra, DifferentialEquations, Plots
+
+function lorenz63!(du, u, p, t)
+	σ, ρ, β = p
+    x, y, z = u
+
+    du[1] = σ * (y - x)
+    du[2] = -x * z + ρ * x - y
+    du[3] = x * y - β * z
+end
+
+function lyapunov(ds, u0, p, Δt, N, δ0)
+	xrefr_prob = ODEProblem(ds, u0,                          (0.0, Δt*N), p)
+	xtest_prob = ODEProblem(ds, u0 .+ (δ0/sqrt(length(u0))), (0.0, Δt*N), p)
+
+	xrefr_int = init(xrefr_prob, Tsit5())
+	xtest_int = init(xtest_prob, Tsit5())
+
+	δi = []
+	for _ in 1:N
+		step!(xrefr_int, Δt, true)
+		step!(xtest_int, Δt, true)
+		push!(δi, norm(xrefr_int.u .- xtest_int.u))
+		reinit!(xtest_int, xrefr_int.u .+ (xtest_int.u .- xrefr_int.u) .* (δ0/δi[end]))
+	end
+
+	λ1 = (1/(N*Δt)) * sum(log.(δi ./ δ0))
+end
+
+u0 = [20.0, 20.0, 20.0]
+p  = [10.0, 28.0, 8/3]
+λ1 = lyapunov(lorenz63!, u0, p, 1.0, 1_000, 0.1)

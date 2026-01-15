810ee0db2fadbd03c94a5c74f11a16b4e46bd505
Ian Wineman <130872935+ianwineman@users.noreply.github.com>
2026-01-14 19:08:06 -0500

    complete exercise 5.5
    Periodic timeseries spectral entropy: 0.0088
    Chaotic timeseries spectral entropy: 0.4773
    Pure noise spectral entropy: 6.4493

---
+++ exercises/5/exercise5_5.jl
@@ -0,0 +1,46 @@
+using FFTW, Statistics
+
+function logistic_map(x; r=3.25)#r=3.58)
+	return r*x*(1-x)
+end
+
+H(probs) = -sum([p*log(p) for p in probs])
+
+# Periodic timeseries
+r = 3.25
+u0 = 0.4
+traj = [u0]
+
+for _ in 1:1000-1
+	push!(traj, logistic_map(traj[end]; r=r))
+end
+
+probs = abs2.(fft(traj .- mean(traj))) 
+probs ./= sum(probs)
+
+printstyled("Periodic timeseries spectral entropy: "; color=:light_magenta)
+printstyled("$(round(H(probs); digits=4))\n"; color=:light_cyan)
+
+# Chaotic timeseries
+r = 3.58
+u0 = 0.4
+traj = [u0]
+
+for _ in 1:1000-1
+	push!(traj, logistic_map(traj[end]; r=r))
+end
+
+probs = abs2.(fft(traj .- mean(traj))) 
+probs ./= sum(probs)
+
+printstyled("Chaotic timeseries spectral entropy: "; color=:light_magenta)
+printstyled("$(round(H(probs); digits=4))\n"; color=:light_cyan)
+
+# Noise timeseries
+traj = rand(1000)
+
+probs = abs2.(fft(traj .- mean(traj))) 
+probs ./= sum(probs)
+
+printstyled("Pure noise spectral entropy: "; color=:light_magenta)
+printstyled("$(round(H(probs); digits=4))\n"; color=:light_cyan)

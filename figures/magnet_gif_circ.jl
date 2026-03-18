09696bb6932fb80c371e24687fc7bd3d273631db
Ian Wineman <130872935+ianwineman@users.noreply.github.com>
2026-03-18 14:47:22 -0400

    .

--- figures/magnet_gif_circ.jl
+++ figures/magnet_gif_circ.jl
@@ -156,6 +156,7 @@ for color in colors
 end
 =#
 
+#=
 # show basins of attraction for m1 as it moves
 using Images
 using DifferentialEquations
@@ -225,3 +226,88 @@ function generate_gif(size, theme; r=2)
 end
 
 generate_gif((1_000, 1_000), :lajolla; r=3)
+=#
+
+#=
+# Generate gif of moving magnets (new) path
+using Plots; ENV["GKSwstype"] = "100"
+
+θ = vcat(range(-π, 0; length=50), range(0, π; length=50))
+anim = @animate for i in 1:100
+	plot(cos.(θ), sin.(θ), aspect_ratio=:equal, lc=:black, lw=1.5, label=false, xlim=[-5, 5], ylim=[-5, 5], xtick=[-5, 0, 5], ytick=[-5, 0, 5])
+	scatter!([-0.5, -0.5], [sqrt(3)/2, -sqrt(3)/2], mc=:blue, ms=6, label=false)
+	plot!(sqrt(0.5) .* cos.(θ) .+ (sqrt(0.5) + 1) , sqrt(0.5) .* sin.(θ), lc=:black, lw=1.5, ls=:dash, label=false)
+	scatter!([sqrt(0.5) * cos(θ[i]) + (sqrt(0.5) + 1)], [sqrt(0.5) * sin(θ[i])], mc=:red, ms=6, label=false)
+end
+gif(anim, "plots/mp/mp_circ_2.gif", fps=30)
+=#
+
+# show basins of attraction for m1 as it moves
+using Images
+using DifferentialEquations
+using Base.Threads
+using ColorSchemes, Colors
+using ProgressMeter
+
+function magnetic_pendulum!(du, u, p , t)
+	ω, q, γ, d, x1, y1 = p
+	x, y, vx, vy = u
+
+	x2, y2 = -0.5, sqrt(3)/2
+	x3, y3 = -0.5, -sqrt(3)/2
+
+	du[1] = vx
+	du[2] = vy
+	du[3] = -(ω^2)*x - q*vx - ((γ*(x-x1))/sqrt((x-x1)^2 + (y-y1)^2 + d^2)^3 + (γ*(x-x2))/sqrt((x-x2)^2 + (y-y2)^2 + d^2)^3 + (γ*(x-x3))/sqrt((x-x3)^2 + (y-y3)^2 + d^2)^3)
+	du[4] = -(ω^2)*y - q*vy - ((γ*(y-y1))/sqrt((x-x1)^2 + (y-y1)^2 + d^2)^3 + (γ*(y-y2))/sqrt((x-x2)^2 + (y-y2)^2 + d^2)^3 + (γ*(y-y3))/sqrt((x-x3)^2 + (y-y3)^2 + d^2)^3)
+end
+
+function trajectory(system, u, tspan, p)
+	prob = ODEProblem(system, u, tspan, p)
+	solu = solve(prob; save_everystep=false, save_start=false)
+	return solu
+end
+
+function closest_magnet(x, y; m1=(1.0, 0.0))
+	x1, y1 = m1
+	x2, y2 = -0.5, sqrt(3)/2
+	x3, y3 = -0.5, -sqrt(3)/2
+	δ = [
+		sqrt((x-x1)^2 + (y-y1)^2), 
+		sqrt((x-x2)^2 + (y-y2)^2), 
+		sqrt((x-x3)^2 + (y-y3)^2)
+	]
+	return argmin(δ)
+end
+
+function generate_image(size, theme; m1=(1.0, 0.0))
+	tspan = (0.0, 100.0)
+	p = [1.0, 0.2, 1.0, 0.3]
+	img = zeros(RGB{Float64}, size...)
+	xs, ys = range(-5.0, 5.0; length=size[1]), range(5.0, -5.0; length=size[2])
+	colors = [get(colorschemes[theme], i) for i in 0.0:0.5:1.0]
+	pm1 = vcat(p, m1...)
+
+	@threads for i in 1:size[1]
+		for j in 1:size[2]
+			tr = trajectory(magnetic_pendulum!, [xs[i], ys[j], 0.0, 0.0], tspan, pm1)
+			cm = closest_magnet(tr.u[end][1:2]...; m1=m1)
+			img[j, i] = colors[cm]
+		end
+	end
+	return img
+end
+
+function generate_gif(size, theme)
+	θs = vcat(range(-π, 0; length=50), range(0, π; length=50))
+	imgs = []
+	@showprogress for θ in θs
+		m1 = sqrt(0.5) * cos(θ) + (sqrt(0.5) + 1), sqrt(0.5) * sin(θ)
+		img = generate_image(size, theme; m1=m1)
+		push!(imgs, img)
+	end
+	save("plots/mp/mp_circ_basins_$(size[1])x$(size[2])_$(string(theme))_tan.gif", cat(imgs..., dims=3); fps=10)
+	println("plots/mp/mp_circ_basins_$(size[1])x$(size[2])_$(string(theme))_tan.gif")
+end
+
+generate_gif((1_000, 1_000), :hawaii)
